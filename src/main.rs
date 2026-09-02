92c646a9f8d1d2468feaaeca7b8944082479b68c
Noticxs <munchablesofficial@outlook.com>
2026-09-02 01:53:29 +0100

    rebuilding search from scratch in rust

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,69 @@
-use miamore::{
-    self, clear_origin, draw_border, draw_text, init_miamore, manage_cursor, position_t,
-    wait_for_seconds,
-};
-use std::sync::Mutex;
+// use miamore::{
+// self, clear_origin, draw_border, draw_text, init_miamore, manage_cursor, position_t,
+// wait_for_seconds,
+// };
 
+use std::fs::File;
+use std::io::{BufReader, prelude::*};
+use std::sync::{LazyLock, Mutex};
+
+#[warn(unused)]
 static ERROR_BUF: Mutex<String> = Mutex::new(String::new());
 
-fn tui_view(_err: &str) {
-    init_miamore(true, true);
-    draw_border("Rust example", miamore::theme_t::thick_l);
+static IGNORE_STACK: LazyLock<Mutex<Vec<String>>> = LazyLock::new(|| Mutex::new(Vec::new()));
 
-    manage_cursor(miamore::cursor_t::move_, Some(position_t { x: 3, y: 4 }));
-    draw_text("Hello from Ametrine!");
+// fn tui_view(_err: &str) {
+//     init_miamore(true, true);
+//     draw_border("Rust example", miamore::theme_t::thick_l);
 
-    wait_for_seconds(8.0);
-    clear_origin();
+//     manage_cursor(miamore::cursor_t::move_, Some(position_t { x: 3, y: 4 }));
+//     draw_text("Hello from Ametrine!");
 
-    let err = "error here";
-    set_error(err);
+//     wait_for_seconds(8.0);
+//     clear_origin();
+
+//     let err = "error here";
+//     set_error(err);
+// }
+
+fn add_ignore_stack(_ignore_file: String) {
+    let mut stack = IGNORE_STACK.lock().unwrap();
+    stack.push(_ignore_file);
+}
+
+fn release_ignore_stack() {
+    let mut stack = IGNORE_STACK.lock().unwrap();
+    while let Some(val) = stack.pop() {
+        println!("Popped: {val}");
+    }
+}
+
+fn read_gitignore() -> std::io::Result<()> {
+    let ignore_file = File::open(".gitignore")?;
+    let reader = BufReader::new(ignore_file);
+
+    for line in reader.lines() {
+        let line = line?;
+        add_ignore_stack(line);
+    }
+
+    Ok(())
 }
 
 fn main() {
-    println!("Search - Ametrine Foundation");
+    #[warn(unused_variables)]
+    let _current_err = ERROR_BUF.lock().unwrap();
 
-    let current_err = ERROR_BUF.lock().unwrap();
-    tui_view(&current_err);
+    if let Err(err) = read_gitignore() {
+        set_error(&err.to_string());
+    }
 
+    // tui_view(&current_err);
+    release_ignore_stack();
     return;
 }
 
 fn set_error(msg: &str) {
-    println!("error -> {}", msg);
+    eprintln!("error -> {}", msg);
+    release_ignore_stack();
 }
