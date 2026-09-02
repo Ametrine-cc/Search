f29804b2f069f1436570103616f25651cd0d92fa
Noticxs <munchablesofficial@outlook.com>
2026-09-02 22:05:12 +0100

    rebuilding search from scratch in rust

--- src/search.rs
+++ src/search.rs
@@ -6,18 +6,33 @@ pub fn search() -> Result<()> {
     println!("searching...");
 
     // List out all files needing to be found
-    {
-        let stack = FILE_STACK.lock().unwrap();
-        for file in stack.iter() {
-            println!("seaching for -> {}", file);
-        }
-    }
+    // {
+    //     let stack = FILE_STACK.lock().unwrap();
+    //     for file in stack.iter() {
+    //         println!("seaching for -> {}", file);
+    //     }
+    // }
 
     // List of files being checked
-    {
-        let stack = CHECK_STACK.lock().unwrap();
-        for file in stack.iter() {
-            println!("seaching through -> {}", file);
+    // {
+    //     let stack = CHECK_STACK.lock().unwrap();
+    //     for file in stack.iter() {
+    //         println!("seaching through -> {}", file);
+    //     }
+    // }
+
+    let file_stack = FILE_STACK.lock().unwrap();
+    let check_stack = CHECK_STACK.lock().unwrap();
+
+    for file in file_stack.iter() {
+        for check_file in check_stack.iter() {
+            if file == check_file {
+                println!("found {}", file);
+            } else if check_file.contains(file) {
+                println!("found {}", check_file);
+            } else {
+                continue;
+            }
         }
     }
 
